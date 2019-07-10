f266352074b0bb66f411354eb5e172b561b18efc
Chris Cheong <chrisxcheong@gmail.com>
2019-07-10 06:33:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
-  if array.size == 1 
+  if array.size == 1
     return array[0]
   elsif array.size == 2
     return "#{array[0]} and #{array[1]}"
   else
     last = array.pop
-    ox_commas = array.map { |e| "#{e}, " } 
+    ox_commas = array.map { |e| "#{e}, " }
     ox_commas << "and #{last}"
     return ox_commas.join
   end
