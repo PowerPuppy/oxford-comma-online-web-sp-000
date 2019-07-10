7a637e30bbcd63869f658226f84f9c4071ed3c70
Chris Cheong <chrisxcheong@gmail.com>
2019-07-10 06:33:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 def oxford_comma(array)
-if array.size == 1
-  array.join(" ,")
-elsif array.size == 2
-  array.join(" and")
-else
-  ox_commas = array.map { |e| "#{e}, " }
-  ox_commas << "and #{last}"
-  return ox_commas.join
-end
-
+  if array.size == 1 
+    return array[0]
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    last = array.pop
+    ox_commas = array.map { |e| "#{e}, " } 
+    ox_commas << "and #{last}"
+    return ox_commas.join
+  end
 end
