f1d38f8b836d7f73fa24a561d08e2dd6165eaf97
Chris Cheong <chrisxcheong@gmail.com>
2019-07-10 06:32:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ if array.size == 1
   array.join(" ,")
 elsif array.size == 2
   array.join(" and")
-else 
-  ox_commas = array.map { |e| "#{e}, " } 
+else
+  ox_commas = array.map { |e| "#{e}, " }
   ox_commas << "and #{last}"
   return ox_commas.join
 end
